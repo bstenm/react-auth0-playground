91604ec9dafe1e55f64c241541f0221c0d4e0889
Bertrand Steinmetz <bstenm@hotmail.com>
2018-08-20 00:19:24 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+      render() {
+            return (
+                  <div className="App">
+                        <header className="App-header">
+                        </header>
+                        <p className="App-intro">
+                        </p>
+                  </div>
+            );
+      }
+}
+
+export default App;
