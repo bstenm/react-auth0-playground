0a60135792fc533be2cf8419526b47132ca18622
Bertrand Steinmetz <bstenm@hotmail.com>
2018-08-27 15:10:54 -0500

    maintenance

--- src/services/Auth0.js
+++ src/services/Auth0.js
@@ -5,13 +5,6 @@ const auth0 = new auth0Js.WebAuth(auth0Config);
 
 export const signIn = () => auth0.authorize();
 
-export const getProfile = (accessToken, cb, cbError) => {
-      auth0.client.userInfo(accessToken, (err, profile) => {
-            if (err) return cbError(err);
-            else cb(profile);
-      });
-};
-
 export const handleAuthentication = (cb, cbError) => {
       auth0.parseHash((err, authResult) => {
             if (err) return cbError(err);
